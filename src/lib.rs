6b1dd0402e27d25ed3b19bf2c5973760cdbb5d75
Sam Sims <samsimss98@gmail.com>
2026-03-17 19:02:59 +0000

    chore: delete old src

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod app;
-pub mod cli;
-mod config;
-pub mod core;
-pub mod logging;
-pub mod overlay;
-pub mod ui;
-pub mod update;
--- src/overlay/overlay_state.rs
+++
@@ -1,8 +0,0 @@
-use super::{CommandPaletteState, MinimapState, Notification};
-
-#[derive(Debug, Default)]
-pub struct OverlayState {
-    pub palette: Option<CommandPaletteState>,
-    pub minimap: Option<MinimapState>,
-    pub notification: Option<Notification>,
-}
