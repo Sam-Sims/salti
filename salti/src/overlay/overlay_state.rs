6a5c655383f7a19d79bdd79d6509b5bcbd35ad70
Sam Sims <samsimss98@gmail.com>
2026-03-18 21:14:00 +0000

    refactor(salti): overlay into single field with active overlay

--- salti/src/overlay/overlay_state.rs
+++ salti/src/overlay/overlay_state.rs
@@ -1,8 +1,30 @@
-use super::{CommandPaletteState, MinimapState, Notification};
+use super::command_palette::CommandPaletteState;
+use super::minimap::MinimapState;
+
+#[derive(Debug)]
+pub enum ActiveOverlay {
+    Palette(CommandPaletteState),
+    Minimap(MinimapState),
+}
 
 #[derive(Debug, Default)]
 pub struct OverlayState {
-    pub palette: Option<CommandPaletteState>,
-    pub minimap: Option<MinimapState>,
-    pub notification: Option<Notification>,
+    pub active_overlay: Option<ActiveOverlay>,
+}
+
+impl OverlayState {
+    pub fn open_palette(&mut self, palette: CommandPaletteState) {
+        self.active_overlay = Some(ActiveOverlay::Palette(palette));
+    }
+
+    pub fn toggle_minimap(&mut self) {
+        self.active_overlay = match self.active_overlay.take() {
+            Some(ActiveOverlay::Minimap(_)) => None,
+            _ => Some(ActiveOverlay::Minimap(MinimapState::default())),
+        };
+    }
+
+    pub fn close(&mut self) {
+        self.active_overlay = None;
+    }
 }
